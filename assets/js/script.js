107b89f377e66db6ac5ec1bf4b4d31865ff15b5f
Thunnder <gnorzea@gmail.com>
2022-04-09 09:56:52 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -187,16 +187,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event, ui) {
-    console.log(ui);
+    $(this).addClass("dropover"),
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function (event, ui) {
-    console.log(ui);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function (event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active")
   },
   out: function (event) {
-    console.log(event);
+    $(event.target).removeClass("dropover-active")
   },
   update: function () {
     var tempArr = [];
@@ -232,10 +234,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   }
 
 });
@@ -276,3 +278,12 @@ var auditTask = function(taskEl) {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+    
+  });
+}, 1800000);
+
+console.log(taskEl)
